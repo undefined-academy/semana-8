261d3db88df44563478b69acbaa05c7e298e8df0
Aitor de Diego <aitordediego23@gmail.com>
2023-06-30 22:56:26 +0200

    Actualizar comentarios correctamente

--- express/aitorias-9443/routes/products.js
+++ express/aitorias-9443/routes/products.js
@@ -3,25 +3,25 @@ const router = express.Router();
 
 const ProductController = require("../controllers/Product");
 
-// Ruta para el listado de usuarios
+// Ruta para el listado de productos
 router.get("/", ProductController.getAllProducts);
 
-// Ruta para la página de creación de usuarios
+// Ruta para la página de creación de productos
 router.get("/create", ProductController.getCreateProductPage);
 
-// Ruta para guardar el nuevo usuario
+// Ruta para guardar el nuevo producto
 router.post("/", ProductController.createProduct);
 
-// Ruta para obtener un usuario por ID
+// Ruta para obtener un producto por ID
 router.get("/:id", ProductController.getProductById);
 
-// Ruta para la página de edición de usuarios
+// Ruta para la página de edición de producto
 router.get("/:id/edit", ProductController.getEditProductPage);
 
-// Ruta para actualizar los datos de un usuario
+// Ruta para actualizar los datos de un producto
 router.post("/:id/edit", ProductController.updateProduct);
 
-// Ruta para eliminar un usuario
+// Ruta para eliminar un producto
 router.post("/:id/delete", ProductController.deleteProduct);
 
 module.exports = router;
